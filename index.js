51d325a1500a0dd3d2a92f8158fa6b6973376696
railsstudent <cleung334@gmail.com>
2020-06-03 13:46:29 +0800

    Initial commit

--- index.js
+++ index.js
@@ -3,21 +3,4 @@ import { render } from 'react-dom';
 import './style.css';
 import { App } from './App'
 
-// class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       name: 'React'
-//     };
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <p>Hello {this.state.name}</p>
-//       </div>
-//     );
-//   }
-// }
-
 render(<App />, document.getElementById('root'));
