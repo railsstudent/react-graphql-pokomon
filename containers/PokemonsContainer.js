1579f023f56538c0992706f4d5692338e61e5d56
railsstudent <cleung334@gmail.com>
2020-06-04 19:34:24 +0800

    Call graphql query in container

--- containers/PokemonsContainer.js
+++ containers/PokemonsContainer.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks'
+import Pokemon from '../components/Pokemon'
 
 export default function PokemonsContainer() {
-  return (<p>I am a pokemon container</p>)
+  const { data: { pokemons = [] } = {} }  = useQuery(GET_POKEMON, {
+    variables: { first: 9 },
+  })
+
+  return (
+    <div className="container">
+      { pokemons && pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />) }
+    </div>
+  )
 }
