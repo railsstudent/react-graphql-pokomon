1e0695371b3de0c39626ad6c58fb37c3aa0f84ca
railsstudent <cleung334@gmail.com>
2020-06-04 20:06:04 +0800

    Design pokemon component

--- components/Pokemon.js
+++ components/Pokemon.js
@@ -1,5 +1,24 @@
 import React from 'react'
 
-export default function Pokemon() {
-  return (<p>I am a pokemon</p>)
+export default function Pokemon({ pokemon }) {
+  console.log('pokemon', pokemon)
+  return (
+    <div className="pokemon">
+      <div className="pokemon__name">
+        {pokemon.name}
+      </div>
+      <div className="pokemon__meta">
+        <span>{pokemon.maxHP}</span>,
+        <span>{pokemon.maxCP}</span>
+      </div>
+      <div className="pokemon__image">
+        <img src={pokemon.image} alt={pokemon.name} />
+      </div>
+      <div className="pokemon__attacks">
+        {
+          pokemon.attacks.special.map(s => <p>{s.name}, {s.damage}</p>)
+        }
+      </div>
+    </div>
+  )
 }
