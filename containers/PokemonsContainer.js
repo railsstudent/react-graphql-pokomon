c0b473003ef24e6a00c3adc0afd3e6d4657d57f8
railsstudent <cleung334@gmail.com>
2020-06-04 19:51:05 +0800

    Define GET_POKEMONS graphql query

--- containers/PokemonsContainer.js
+++ containers/PokemonsContainer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks'
 import Pokemon from '../components/Pokemon'
+import { GET_POKEMONS } from '../graphql/get-pokemons'
 
 export default function PokemonsContainer() {
-  const { data: { pokemons = [] } = {} }  = useQuery(GET_POKEMON, {
+  const { data: { pokemons = [] } = {} }  = useQuery(GET_POKEMONS, {
     variables: { first: 9 },
   })
 
