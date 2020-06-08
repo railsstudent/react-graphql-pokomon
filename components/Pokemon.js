4b69846e3c72f2fb2a3d857c2e9d0f20376011a4
railsstudent <cleung334@gmail.com>
2020-06-08 14:21:39 +0800

    Fix pokemon attack elements

--- components/Pokemon.js
+++ components/Pokemon.js
@@ -8,17 +8,20 @@ export default function Pokemon({ pokemon }) {
         {pokemon.name}
       </div>
       <div className="pokemon__meta">
-        <span>{pokemon.maxHP}</span>,
+        <span>{pokemon.maxHP}</span>
         <span>{pokemon.maxCP}</span>
       </div>
       <div className="pokemon__image">
         <img src={pokemon.image} alt={pokemon.name} />
       </div>
+
       <div className="pokemon__attacks">
-        {
-          pokemon.attacks.special.map(s => <p>{s.name}, {s.damage}</p>)
+        { 
+          pokemon.attacks.special.slice(0, 3).map(attack => 
+          <span key={`${attack.name}-${attack.damage}`}>{attack.name}</span>)
         }
       </div>
+
     </div>
   )
 }
